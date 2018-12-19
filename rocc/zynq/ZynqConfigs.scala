efe2d947268e8a9979c4769fa57d2cf2bdd2b957
Josh Heidecker <josheidecker@gmail.com>
2018-12-19 16:50:43 -0500

    Descriptions of ZynqConfigs added.

--- rocc/zynq/ZynqConfigs.scala
+++ rocc/zynq/ZynqConfigs.scala
@@ -8,7 +8,10 @@ import freechips.rocketchip.rocket.{RocketCoreParams, MulDivParams, DCacheParams
 import freechips.rocketchip.tile.{RocketTileParams, BuildCore, XLen}
 import testchipip._
 
+// Use this config as a base for adding accelerators to your rocket-chip
 class ZynqFPGAAcceleratorConfig extends Config(new WithoutTLMonitors ++ new WithZynqAdapter ++ new WithBootROM)
 
+// This config produces a rocket-chip for a Zynq FPGA using a custom accelerator whose config is MultAcceleratorConfig
+// Configs for other accelerators will look similar to this one e.g.
+// class YourConfig extends Config(new ZynqFPGAAcceleratorConfig ++ new freechips.rocketchip.system.YourAcceleratorConfig)
 class ZynqFPGAMultAcceleratorConfig extends Config(new ZynqFPGAAcceleratorConfig ++ new freechips.rocketchip.system.MultAcceleratorConfig)
-
