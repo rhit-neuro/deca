f14e8aede35cc7305a463f4b4ab177ccb8af6acf
Josh Heidecker <josheidecker@gmail.com>
2019-02-01 11:52:05 -0500

    ZynqFPGAAccleratorConfig is the same as ZynqFPGAConfig

--- rocc/zynq/ZynqConfigs.scala
+++ rocc/zynq/ZynqConfigs.scala
@@ -8,14 +8,11 @@ import freechips.rocketchip.tile.{RocketTileParams, BuildCore, XLen}
 import testchipip._
 import freechips.rocketchip.coreplex.WithoutTLMonitors
 
-// Use this config as a base for adding accelerators to your rocket-chip
-class ZynqFPGAAcceleratorConfig extends Config(new WithoutTLMonitors ++ new WithZynqAdapter ++ new WithBootROM)
-
 // This config produces a rocket-chip for a Zynq FPGA using a custom accelerator whose config is MultAcceleratorConfig
 // Configs for other accelerators will look similar to this one e.g.
-// class YourConfig extends Config(new ZynqFPGAAcceleratorConfig ++ new freechips.rocketchip.system.YourAcceleratorConfig)
-class ZynqFPGAMultAcceleratorConfig extends Config(new ZynqFPGAAcceleratorConfig ++ new freechips.rocketchip.system.MultAcceleratorConfig)
+// class YourConfig extends Config(new ZynqFPGAConfig ++ new freechips.rocketchip.system.YourAcceleratorConfig)
+class ZynqFPGAMultAcceleratorConfig extends Config(new ZynqFPGAConfig ++ new freechips.rocketchip.system.MultAcceleratorConfig)
 
 // This config produces a rocket-chip for a Zynq FPGA using a custom accelerator whose config is LUTROMAcceleratorConfig
-class ZynqFPGALUTROMAcceleratorConfig extends Config(new ZynqFPGAAcceleratorConfig ++ new freechips.rocketchip.system.LUTROMAcceleratorConfig)
+class ZynqFPGALUTROMAcceleratorConfig extends Config(new ZynqFPGAConfig ++ new freechips.rocketchip.system.LUTROMAcceleratorConfig)
 
